14dcbaa9f347137631c9ce0acef2c63fa5d1e7d7
xieqiang <xieqiang@wdai.com>
2018-09-06 09:38:07 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		//配置中心
+		SpringApplication.run(Application.class, args);
+	}
+}
